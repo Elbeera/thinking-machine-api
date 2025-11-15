c6c2e4fabe7af03bf41e7a438ffae76bb2a00502
Hothyfa-Elbeera <hothyfa.elbeera@claimsgate.co.uk>
2025-11-15 13:14:34 +0000

    Add UploadCsv function to handle file uploads

--- function_app.py
+++ function_app.py
@@ -4,3 +4,24 @@ import json
 import logging
 
 app = func.FunctionApp()
+
+@app.route(route="UploadCsv", auth_level=func.AuthLevel.ANONYMOUS)
+def UploadCsv(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info(f'Uploading file: {file_name}')
+
+    file_name = req.params.get('file_name')
+    if not file_name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            file_name = req_body.get('file_name')
+
+    if file_name:
+        return func.HttpResponse(f"Successfully uploaded {file_name}.", status_code=200)
+    else:
+        return func.HttpResponse(
+             "Failed to upload file. Pass a file_name in the request body.",
+             status_code=400
+        )
